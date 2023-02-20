2413030fbd7c3453407ff8d4d9562ae534b2d79b
Emmanuelbreezy <emmabreezyumeh@gmail.com>
2023-02-21 00:45:12 +0100

    first commit

---
+++ src/Button.tsx
@@ -0,0 +1,25 @@
+import React, { HTMLAttributes } from 'react';
+
+export interface Props extends HTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+  variant: 'primary' | 'secondary';
+}
+
+export const Button = ({ children, variant = 'primary', ...props }: Props) => {
+  return (
+    <button
+      {...props}
+      style={{
+        backgroundColor: variant === 'primary' ? 'blue' : 'gray',
+        color: 'white',
+        outline: 'none',
+        border: 'none',
+        borderRadius: 100,
+        padding: 10,
+        cursor: 'pointer',
+      }}
+    >
+      {children}
+    </button>
+  );
+};
